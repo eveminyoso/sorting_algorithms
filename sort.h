37862166213d38a0d39a99fcaba9ea6f719b8411
eveminyoso <eveminyoso@gmail.com>
2023-12-17 10:55:12 -0800

    sorting_algorithms

--- sort.h
+++ sort.h
@@ -4,23 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* comparison direction macros for bitonic sort */
-#define UP 0
-#define DOWN 1
-
 /**
- * enum boo1 - Enumeration of boolean values.
- * @false: Equals 0
- * @true: Equals 1
- */
-typedef enum boo1
-{
-	false = 0
-	true
-} boo1;
-
-/**
- * * struct listint_s - Doubly linked list node
+ * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
@@ -28,11 +13,24 @@ typedef enum boo1
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/*
- * printing helper function */
-void print array(const int *array, size_t size)
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void swap(int *a, int *b);
+void bubble_sort(int *array, size_t size);
+
+void insertion_sort_list(listint_t **list);
+
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
+#endif
