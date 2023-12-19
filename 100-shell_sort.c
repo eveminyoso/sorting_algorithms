51ee380ca1cce80eca955f0973608d732fc99667
eveminyoso <eveminyoso@gmail.com>
2023-12-19 13:20:08 -0800

    100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,56 +1,31 @@
-#include <stdio.h>
-
-void print_array(int *array, size_t size)
-{
-	for (size_t i = 0; i < size; i++)
-	{
-		printf("%d ", array[i]);
-	}
-	printf("\n");
-}
-
+#include "sort.h"
+/**
+ * shell_sort - Sorts an array of integers in ascending order
+ * using the Shell sort algorithm with the Knuth sequence.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1;
+	size_t i, j, gap = 1;
+	int temp;
 
-	while (gap <= size / 3)
+	if (array == NULL || size < 2)
+		return;
+	while (gap < size)
 	{
 		gap = gap * 3 + 1;
-	}
-
-	while (gap > 0)
-	{
-		for (size_t i = gap; i < size; i++)
+		for (i = gap; i < size; i++)
 		{
-			int temp = array[i];
-			size_t j;
-
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			temp = array[i];
+			j = i;
+			while (j >= gap && array[j - gap] > temp)
 			{
 				array[j] = array[j - gap];
+				j -= gap;
 			}
 			array[j] = temp;
 		}
-
 		print_array(array, size);
-
-		gap = (gap - 1) / 3;
 	}
-}
-
-int main void
-{
-
-	int array[] = {64, 25, 12, 22, 11};
-	size_t size = sizeof(array) / sizeof(array[0]);
-
-	printf("Original array: ");
-	print_array(array, size);
-
-	shell_sort(array, size);
-
-	printf("Sorted array: ");
-	print_array(array, size);
-
-	return (0);
 }
